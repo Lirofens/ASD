46f948e28c6e8a8ddcdc4a404a486d4d9f4d29ae
Lirofens <Lirofens24@gmail.com>
2025-10-11 21:02:55 +0300

    Completing the writing of the Stack class and covering it with tests.

--- lib_stack/stack.h
+++ lib_stack/stack.h
@@ -7,14 +7,12 @@ private:
 	size_t _size;
 	int _top;
 public:
-	Stack(size_t size = 20) : _size(size), _top(-1) {
-		_data = new T[size];
-	}
+	Stack(size_t size = 20) : _data (new T[size]), _size(size), _top(-1) {}
 	~Stack() {
 		delete[] _data;
 		_data = nullptr;
 	}
-	inline void push(T& el) { 
+	inline void push(const T& el) { 
 		if (is_full()) throw std::out_of_range("Stack is full!");
 		_data[++_top] = el;
 	}
@@ -26,6 +24,6 @@ public:
 		if (is_empty()) throw std::out_of_range("Stack is empty!");
 		return _data[_top];
 	}
-	inline bool is_empty() { return _top == -1; }
-	inline bool is_full() { return _top == -1; }
+	inline bool is_empty() const noexcept { return _top == -1; }
+	inline bool is_full() const noexcept { return (_top + 1) == _size; }
 };
--- tests/test_stack.cpp
+++ tests/test_stack.cpp
@@ -8,3 +8,62 @@
 TEST(TestStackLib, can_create_with_def_constr) {
     ASSERT_NO_THROW(Stack<int> a);
 }
+
+TEST(TestStackLib, can_create_with_specific_size) {
+    ASSERT_NO_THROW(Stack<int> a(10));
+}
+
+TEST(TestStackLib, can_push_correctly) {
+    Stack<double> s;
+    s.push(0.0);
+    s.push(1.0);
+    s.push(-1.0);
+
+    EXPECT_EQ(s.top(), -1.0);
+}
+
+TEST(TestStackLib, can_pop_correctly) {
+    Stack<double> s;
+    s.push(0.0);
+    s.push(1.0);
+    s.push(-1.0);
+    s.pop();
+
+    EXPECT_EQ(s.top(), 1.0);
+}
+
+TEST(TestStackLib, can_push_and_pop_correctly) {
+    Stack<double> s;
+    s.push(0.0);
+    s.push(1.0);
+    s.push(-1.0);
+    s.pop();
+    s.push(-1.0);
+    s.push(2.0);
+    s.push(4.0);
+    s.pop();
+    s.push(4.0);
+    s.pop();
+
+    EXPECT_EQ(s.top(), 2.0);
+}
+
+TEST(TestStackLib, throw_is_full_func_correctly) {
+    Stack<double> s(1);
+    s.push(0.0);
+    ASSERT_ANY_THROW(s.push(0.0));
+}
+
+TEST(TestStackLib, throw_is_empty_func_correctly) {
+    Stack<double> s(3);
+    s.push(0.0);
+    s.pop();
+    ASSERT_ANY_THROW(s.pop());
+}
+
+TEST(TestStackLib, throw_top_func_correctly) {
+    Stack<double> s(3);
+    s.push(0.0);
+    s.pop();
+    ASSERT_ANY_THROW(s.top());
+}
